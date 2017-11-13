ad130287882e788e39b64586a1c54a492567a856
HaiwangYu <yuhw.pku@gmail.com>
2017-11-13 18:40:23 -0500

    init macro for ReadMySql module

---
+++ fun4all/macros/Fun4ReadMySql.C
@@ -0,0 +1,34 @@
+/*
+ * Fun4ReadMySql.C
+ *
+ *  Created on: Oct 30, 2017
+ *      Author: yuhw
+ */
+
+
+
+void Fun4ReadMySql(
+const int nevent = 1, 
+const char *outfile = "out.root"
+) {
+
+	gSystem->Load("libfun4all.so");
+	Fun4AllServer* se = Fun4AllServer::instance();
+
+	gSystem->Load("libmodules.so");
+	ReadMySql *simple_reader = new ReadMySql();
+    simple_reader->Verbosity(100);
+
+    Fun4AllInputManager *in = new Fun4AllDummyInputManager("JADE");
+	se->registerInputManager(in);
+
+	Fun4AllDstOutputManager *out = new Fun4AllDstOutputManager("DSTOUT", outfile);
+	se->registerOutputManager(out);
+
+	se->registerSubsystem(simple_reader);
+
+	se->run(nevent);
+	se->End();
+
+	delete se;
+}
