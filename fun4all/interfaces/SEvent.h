6e33a208e63282f39b084306c1587f03ef46c4a0
HaiwangYu <yuhw.pku@gmail.com>
2017-11-14 11:39:15 -0500

    Change SEvent to store event header info

--- fun4all/interfaces/SEvent.h
+++ fun4all/interfaces/SEvent.h
@@ -18,6 +18,20 @@
 class SEvent: public PHObject {
 
 public:
+
+	enum TriggerMask {
+		NIM1 = 0,
+		NIM2 = 1,
+		NIM3 = 2,
+		NIM4 = 3,
+		NIM5 = 4,
+		MATRIX1 = 5,
+		MATRIX2 = 6,
+		MATRIX3 = 7,
+		MATRIX4 = 8,
+		MATRIX5 = 9
+	};
+
 	virtual ~SEvent() {}
 
 	void Reset() {}
@@ -28,8 +42,35 @@ public:
 		<< std::endl;
 	}
 
-	virtual void SetSHitMap(SHitMap* a) = 0;
-	virtual SHitMap* GetSHitMap() = 0;
+	virtual int get_run_id() const = 0;
+	virtual void set_run_id(const int a) = 0;
+
+	virtual int get_spill_id() const = 0;
+	virtual void set_spill_id(const int a) = 0;
+
+	virtual int get_event_id() const = 0;
+	virtual void set_event_id(const int a) = 0;
+
+	virtual int get_coda_event_id() const = 0;
+	virtual void set_coda_event_id(const int a) = 0;
+
+
+	virtual int get_data_quality() const = 0;
+	virtual void set_data_quality(const int a) = 0;
+
+	virtual int get_vme_time() const = 0;
+	virtual void set_vme_time(const int a) = 0;
+
+	virtual bool get_trigger(const SEvent::TriggerMask i) const = 0;
+	virtual void set_trigger(const SEvent::TriggerMask i, const bool a) = 0;
+
+	virtual int get_raw_matrix(const unsigned short i) const = 0;
+	virtual void set_raw_matrix(const unsigned short i, const bool a) = 0;
+
+	virtual int get_after_inh_matrix(const unsigned short i) const = 0;
+	virtual void set_after_inh_matrix(const unsigned short i, const bool a) = 0;
+
+
 
 protected:
 
--- fun4all/interfaces/SEvent_v1.C
+++ fun4all/interfaces/SEvent_v1.C
@@ -16,8 +16,16 @@ using namespace std;
 ClassImp(SEvent_v1)
 
 SEvent_v1::SEvent_v1() :
-_hitmap(nullptr)
+_run_id(INT_MAX),
+_spill_id(INT_MAX),
+_event_id(INT_MAX),
+_coda_event_id(INT_MAX),
+_trigger(0),
+_data_quality(INT_MAX),
+_vme_time(INT_MAX)
 {
+	for(int i=0;i<5;++i) _raw_matrix[i] = 0;
+	for(int i=0;i<5;++i) _after_inh_matrix[i] = 0;
 }
 
 SEvent_v1::~SEvent_v1() {
@@ -25,26 +33,24 @@ SEvent_v1::~SEvent_v1() {
 }
 
 void SEvent_v1::Reset() {
-
-	if(_hitmap) {
-		delete _hitmap;
-	}
-
 	return;
 }
 
 void SEvent_v1::identify(std::ostream& os) const {
 	  cout << "---SEvent_v1::identify:--------------------------" << endl;
-
-	  if(_hitmap) {
-		  cout << "\t ---Contains SHitMap:-------------------" << endl;
-		  _hitmap->identify();
+	  cout
+	  << "runID: " << _run_id
+	  << "spillID: " << _spill_id
+	  << "eventID: " << _event_id
+	  << "codaEventID: " << _coda_event_id
+	  << endl;
+	  for(int i = SEvent::NIM1; i<=SEvent::NIM5; ++i) {
+		  cout <<"NIM"<<i<<": " << get_trigger(static_cast<SEvent::TriggerMask>(i)) << "; ";
 	  }
-
+	  cout <<endl;
+	  for(int i = SEvent::MATRIX1; i<=SEvent::MATRIX5; ++i){
+		  cout <<"MATRIX"<<i<<": " << get_trigger(static_cast<SEvent::TriggerMask>(i)) << "; ";
+	  }
+	  cout <<endl;
 	  return;
-}
-
-void SEvent_v1::SetSHitMap(SHitMap* a) {
-	if(_hitmap) delete _hitmap;
-	_hitmap = a;
 }
