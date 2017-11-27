58e444925dede7650c9730290666edd5e7314081
HaiwangYu <yuhw.pku@gmail.com>
2017-11-26 22:48:47 -0500

    update with HitVector

--- fun4all/macros/Fun4ReadMySql.C
+++ fun4all/macros/Fun4ReadMySql.C
@@ -2,24 +2,28 @@
  * Fun4ReadMySql.C
  *
  *  Created on: Oct 30, 2017
- *      Author: yuhw
+ *      Author: yuhw@nmsu.edu
  */
 
 
 
 void Fun4ReadMySql(
-const int nevent = 1, 
-const char *outfile = "out.root"
-) {
+		const int nevent = 2, 
+		const char *outfile = "out.root"
+		) {
+
+	int verbosity = 0;
 
 	gSystem->Load("libfun4all.so");
 	Fun4AllServer* se = Fun4AllServer::instance();
+	se->Verbosity(verbosity);
 
 	gSystem->Load("libmodules.so");
 	ReadMySql *simple_reader = new ReadMySql();
-    simple_reader->Verbosity(100);
+	simple_reader->Verbosity(verbosity);
+	simple_reader->set_hit_container_choice("Map");
 
-    Fun4AllInputManager *in = new Fun4AllDummyInputManager("JADE");
+	Fun4AllInputManager *in = new Fun4AllDummyInputManager("JADE");
 	se->registerInputManager(in);
 
 	Fun4AllDstOutputManager *out = new Fun4AllDstOutputManager("DSTOUT", outfile);
