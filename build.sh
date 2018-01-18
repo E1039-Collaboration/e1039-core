fa6bbbb4ccdf22b1063569b2a1773c858094363f
HaiwangYu <yuhw.pku@gmail.com>
2018-01-18 15:22:07 -0500

    script to build multiple packages

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+src=/home/yuhw/GitHub/SeaQuest/seaquest-offline
+build=`pwd`
+install=$OFFLINE_MAIN
+
+declare -a packages=(
+		framework/phool
+		framework/ffaobjects
+		framework/fun4all
+		interface_main
+		)
+
+for package in "${packages[@]}"
+do
+	echo $src/$package
+	mkdir -p $build/$package
+	cd $build/$package
+	echo cmake -DCMAKE_INSTALL_PREFIX=$install $src/$package
+	cmake -DCMAKE_INSTALL_PREFIX=$install $src/$package
+	make -j4 install
+done
+
+cd $build
