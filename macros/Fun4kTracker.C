4cd3932dbff3ab8dddb4e8a479a9294213798260
HaiwangYu <yuhw.pku@gmail.com>
2018-02-01 13:37:41 -0500

    macro to use kTracker

---
+++ macros/Fun4kTracker.C
@@ -0,0 +1,36 @@
+/*
+ * Fun4TestAnalyzer.C
+ *
+ *  Created on: Oct 30, 2017
+ *      Author: yuhw@nmsu.edu
+ */
+
+
+
+void Fun4TestAnalyzer(
+		const int nevent = 2,
+		const char *indst = "dst.root",
+		const char *outeval = "eval.root"
+		) {
+
+	int verbosity = 0;
+
+	gSystem->Load("libfun4all.so");
+	Fun4AllServer* se = Fun4AllServer::instance();
+	se->Verbosity(verbosity);
+
+	gSystem->Load("libktracker.so");
+	KalmanFastTrackingWrapper *analyzer = new KalmanFastTrackingWrapper();
+	se->registerSubsystem(analyzer);
+
+	Fun4AllInputManager *in = new Fun4AllDstInputManager("DSTIN");
+	in->fileopen(indst);
+	se->registerInputManager(in);
+
+	se->run(nevent);
+	se->End();
+
+	delete se;
+
+	cout << "Fun4TestAnalyzer Done!" << endl;
+}
