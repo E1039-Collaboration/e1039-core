16643cc97b273c71fa23c506ddf48d288040c3b6
HaiwangYu <yuhw.pku@gmail.com>
2018-05-07 23:42:56 -0400

    keep basic functions

--- simulation/g4detectors/PHG4Parameters.h
+++ simulation/g4detectors/PHG4Parameters.h
@@ -6,8 +6,8 @@
 #include <map>
 #include <string>
 
-class PdbParameterMap;
-class PdbParameterMapContainer;
+//class PdbParameterMap;
+//class PdbParameterMapContainer;
 class PHCompositeNode;
 
 // contains parameters in our units,
@@ -34,7 +34,7 @@ class PHG4Parameters: public PHObject
   void Print() const;
 
   //! hash of binary information for checking purpose
-  size_t get_hash() const;
+//  size_t get_hash() const;
 
   void set_int_param(const std::string &name, const int ival);
   int get_int_param(const std::string &name) const;
@@ -54,19 +54,19 @@ class PHG4Parameters: public PHObject
   void set_name(const std::string &name) {detname = name;}
   std::string Name() const {return detname;}
 
-  void FillFrom(const PdbParameterMap *saveparams);
-  void FillFrom(const PdbParameterMapContainer *saveparamcontainer, const int layer);
+//  void FillFrom(const PdbParameterMap *saveparams);
+//  void FillFrom(const PdbParameterMapContainer *saveparamcontainer, const int layer);
   void FillFrom(const PHG4Parameters *saveparams);
   // save parameters on node tree
   void SaveToNodeTree(PHCompositeNode *topNode, const std::string &nodename);
   // save parameters in container on node tree
   void SaveToNodeTree(PHCompositeNode *topNode, const std::string &nodename, const int layer);
-  int WriteToDB();
-  int ReadFromDB();
-  int ReadFromDB(const std::string &name, const int layer);
-  int WriteToFile(const std::string &extension, const std::string &dir = ".");
-  int ReadFromFile(const std::string &name, const std::string &extension, const int layer, const int issuper, const std::string &dir = ".");
-  void CopyToPdbParameterMap(PdbParameterMap *myparm);
+//  int WriteToDB();
+//  int ReadFromDB();
+//  int ReadFromDB(const std::string &name, const int layer);
+//  int WriteToFile(const std::string &extension, const std::string &dir = ".");
+//  int ReadFromFile(const std::string &name, const std::string &extension, const int layer, const int issuper, const std::string &dir = ".");
+//  void CopyToPdbParameterMap(PdbParameterMap *myparm);
 
   void printint() const;
   void printdouble() const;
@@ -75,7 +75,7 @@ class PHG4Parameters: public PHObject
  protected:
 
   unsigned int ConvertStringToUint(const std::string &str) const;
-  PdbParameterMap *pdbparam;
+//  PdbParameterMap *pdbparam;
   std::string detname;
   dMap doubleparams;
   iMap intparams;
