2bc7f654ae89020d543e144a099f33ae00764291
HaiwangYu <yuhw.pku@gmail.com>
2018-06-11 11:08:14 -0500

    add test analyzer

--- macros/Fun4Sim.C
+++ macros/Fun4Sim.C
@@ -5,49 +5,49 @@ using namespace std;
 
 int Fun4Sim(const int nEvents = 1)
 {
-	const int use_g4steps = 1;
-	const double target_l = 7.9; //cm
-	const double target_z = (7.9-target_l)/2.; //cm
+  const int use_g4steps = 1;
+  const double target_l = 7.9; //cm
+  const double target_z = (7.9-target_l)/2.; //cm
 
-	gSystem->Load("libfun4all");
-	gSystem->Load("libg4detectors");
-	gSystem->Load("libg4testbench");
-	gSystem->Load("libg4eval");
-	gSystem->Load("libtruth_eval.so");
+  gSystem->Load("libfun4all");
+  gSystem->Load("libg4detectors");
+  gSystem->Load("libg4testbench");
+  gSystem->Load("libg4eval");
+  gSystem->Load("libtruth_eval.so");
 
-	///////////////////////////////////////////
-	// Make the Server
-	//////////////////////////////////////////
-	Fun4AllServer *se = Fun4AllServer::instance();
-	se->Verbosity(100);
+  ///////////////////////////////////////////
+  // Make the Server
+  //////////////////////////////////////////
+  Fun4AllServer *se = Fun4AllServer::instance();
+  se->Verbosity(100);
 
-	// particle gun
-	PHG4ParticleGun *gun = new PHG4ParticleGun("PGUN");
-	gun->set_name("mu+");//proton
-	gun->set_vtx(0, 0, -400); //-363.32 cm
-	gun->set_mom(0, 0, 120);
-	TF2 *beam_profile = new TF2("beam_profile",
-			//"(((x**2+y**2)<=0.81)*exp(-(x**2+y**2)/0.18))+(((x**2+y**2)>0.81&&(x**2+y**2)<=25&&abs(y)<1.)*0.9*exp(-4.5)/(sqrt(x**2+y**2)))",
-			"(((x**2+y**2)<=0.81)*exp(-(x**2+y**2)/0.18))+(((x**2+y**2)>0.81&&(x**2+y**2)<=25)*0.9*exp(-4.5)/(sqrt(x**2+y**2)))",
-			-5,5,-5,5);
-	//gun->set_beam_profile(beam_profile);
-	se->registerSubsystem(gun);
+  // particle gun
+  PHG4ParticleGun *gun = new PHG4ParticleGun("PGUN");
+  gun->set_name("mu+");//proton
+  gun->set_vtx(0, 0, -400); //-363.32 cm
+  gun->set_mom(0, 0, 120);
+  TF2 *beam_profile = new TF2("beam_profile",
+      //"(((x**2+y**2)<=0.81)*exp(-(x**2+y**2)/0.18))+(((x**2+y**2)>0.81&&(x**2+y**2)<=25&&abs(y)<1.)*0.9*exp(-4.5)/(sqrt(x**2+y**2)))",
+      "(((x**2+y**2)<=0.81)*exp(-(x**2+y**2)/0.18))+(((x**2+y**2)>0.81&&(x**2+y**2)<=25)*0.9*exp(-4.5)/(sqrt(x**2+y**2)))",
+      -5,5,-5,5);
+  //gun->set_beam_profile(beam_profile);
+  se->registerSubsystem(gun);
 
-	// Fun4All G4 module
-	PHG4Reco *g4Reco = new PHG4Reco();
-	//g4Reco->G4Seed(123);
-	//g4Reco->set_field(5.);
-	g4Reco->set_field_map("target.field.root",4);
-	// size of the world - every detector has to fit in here
-	g4Reco->SetWorldSizeX(1000);
-	g4Reco->SetWorldSizeY(1000);
-	g4Reco->SetWorldSizeZ(5000);
-	// shape of our world - it is a tube
-	g4Reco->SetWorldShape("G4BOX");
-	// this is what our world is filled with
-	g4Reco->SetWorldMaterial("G4_Galactic");
-	// Geant4 Physics list to use
-	g4Reco->SetPhysicsList("FTFP_BERT");
+  // Fun4All G4 module
+  PHG4Reco *g4Reco = new PHG4Reco();
+  //g4Reco->G4Seed(123);
+  //g4Reco->set_field(5.);
+  g4Reco->set_field_map("target.field.root",4);
+  // size of the world - every detector has to fit in here
+  g4Reco->SetWorldSizeX(1000);
+  g4Reco->SetWorldSizeY(1000);
+  g4Reco->SetWorldSizeZ(5000);
+  // shape of our world - it is a tube
+  g4Reco->SetWorldShape("G4BOX");
+  // this is what our world is filled with
+  g4Reco->SetWorldMaterial("G4_Galactic");
+  // Geant4 Physics list to use
+  g4Reco->SetPhysicsList("FTFP_BERT");
 
 
   PHG4E1039InsensSubsystem* insens = new PHG4E1039InsensSubsystem("Insens");
@@ -59,67 +59,74 @@ int Fun4Sim(const int nEvents = 1)
   gROOT->LoadMacro("G4_DriftChamber.C");
   SetupDriftChamber(g4Reco);
 
-	PHG4TruthSubsystem *truth = new PHG4TruthSubsystem();
-	g4Reco->registerSubsystem(truth);
+  PHG4TruthSubsystem *truth = new PHG4TruthSubsystem();
+  g4Reco->registerSubsystem(truth);
 
-	se->registerSubsystem(g4Reco);
+  se->registerSubsystem(g4Reco);
 
   DPDigitizer *digitizer = new DPDigitizer();
   digitizer->Verbosity(100);
   se->registerSubsystem(digitizer);
 
-	//TruthEval* eval = new TruthEval("TruthEval","eval.root");
-	//eval->target_l = target_l;
-	//eval->target_z = target_z;
-	//se->registerSubsystem(eval);
+  gSystem->Load("libmodule_example.so");
+  TestAnalyzer *analyzer = new TestAnalyzer();
+  analyzer->Verbosity(100);
+  analyzer->set_hit_container_choice("Vector");
+  analyzer->set_out_name("test_analyzer.root");
+  se->registerSubsystem(analyzer);
 
-	///////////////////////////////////////////
-	// Output
-	///////////////////////////////////////////
+  //TruthEval* eval = new TruthEval("TruthEval","eval.root");
+  //eval->target_l = target_l;
+  //eval->target_z = target_z;
+  //se->registerSubsystem(eval);
 
-	// save a comprehensive  evaluation file
-	PHG4DSTReader *ana = new PHG4DSTReader(
-			string("DSTReader.root"));
-	ana->set_save_particle(true);
-	ana->set_load_all_particle(false);
-	ana->set_load_active_particle(true);
-	ana->set_save_vertex(true);
-	ana->AddNode("Coil");
-	ana->AddNode("Target");
-	//ana->AddNode("Collimator");
-	ana->AddNode("C1X");
-	ana->AddNode("C2X");
-	se->registerSubsystem(ana);
+  ///////////////////////////////////////////
+  // Output
+  ///////////////////////////////////////////
 
-	// input - we need a dummy to drive the event loop
-	Fun4AllInputManager *in = new Fun4AllDummyInputManager("JADE");
-	se->registerInputManager(in);
+  // save a comprehensive  evaluation file
+  PHG4DSTReader *ana = new PHG4DSTReader(
+      string("DSTReader.root"));
+  ana->set_save_particle(true);
+  ana->set_load_all_particle(false);
+  ana->set_load_active_particle(true);
+  ana->set_save_vertex(true);
+  ana->AddNode("Coil");
+  ana->AddNode("Target");
+  //ana->AddNode("Collimator");
+  ana->AddNode("C1X");
+  ana->AddNode("C2X");
+  se->registerSubsystem(ana);
 
-	Fun4AllDstOutputManager *out = new Fun4AllDstOutputManager("DSTOUT", "DST.root");
-	se->registerOutputManager(out);
+  // input - we need a dummy to drive the event loop
+  Fun4AllInputManager *in = new Fun4AllDummyInputManager("JADE");
+  se->registerInputManager(in);
 
-	// a quick evaluator to inspect on hit/particle/tower level
+  Fun4AllDstOutputManager *out = new Fun4AllDstOutputManager("DSTOUT", "DST.root");
+  se->registerOutputManager(out);
 
-	if (nEvents > 0)
-	{
-		se->run(nEvents);
+  // a quick evaluator to inspect on hit/particle/tower level
 
-		PHGeomUtility::ExportGeomtry(se->topNode(),"geom.root");
+  if (nEvents > 0)
+  {
+    se->run(nEvents);
 
-		// finish job - close and save output files
-		se->End();
-		std::cout << "All done" << std::endl;
+    PHGeomUtility::ExportGeomtry(se->topNode(),"geom.root");
 
-		// cleanup - delete the server and exit
-		delete se;
-		gSystem->Exit(0);
-	}
-	return;
+    // finish job - close and save output files
+    se->End();
+    std::cout << "All done" << std::endl;
+
+    // cleanup - delete the server and exit
+    delete se;
+    gSystem->Exit(0);
+  }
+  return;
 }
 
 PHG4ParticleGun *get_gun(const char *name = "PGUN")
 {
-	Fun4AllServer *se = Fun4AllServer::instance();
-	PHG4ParticleGun *pgun = (PHG4ParticleGun *) se->getSubsysReco(name);
-	return pgun;
+  Fun4AllServer *se = Fun4AllServer::instance();
+  PHG4ParticleGun *pgun = (PHG4ParticleGun *) se->getSubsysReco(name);
+  return pgun;
 }
