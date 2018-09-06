e9f3694d49b8545fee02410cc2498c2bf291a24a
HaiwangYu <yuhw.pku@gmail.com>
2018-09-06 00:32:10 -0500

    add SquareTube detector

---
+++ simulation/g4detectors/PHG4SquareTubeDetector.cc
@@ -0,0 +1,169 @@
+#include "PHG4SquareTubeDetector.h"
+
+#include <phparameter/PHParameters.h>
+
+#include <g4main/PHG4Utils.h>
+
+
+#include <phool/PHCompositeNode.h>
+#include <phool/PHIODataNode.h>
+#include <phool/getClass.h>
+
+#include <Geant4/G4Box.hh>
+#include <Geant4/G4SubtractionSolid.hh>
+#include <Geant4/G4Colour.hh>
+#include <Geant4/G4LogicalVolume.hh>
+#include <Geant4/G4Material.hh>
+#include <Geant4/G4PVPlacement.hh>
+#include <Geant4/G4SystemOfUnits.hh>
+#include <Geant4/G4UserLimits.hh>
+#include <Geant4/G4VisAttributes.hh>
+
+#include <sstream>
+
+using namespace std;
+
+//_______________________________________________________________
+PHG4SquareTubeDetector::PHG4SquareTubeDetector( PHCompositeNode *Node, PHParameters *parameters, const std::string &dnam, const int lyr):
+  PHG4Detector(Node, dnam),
+  params(parameters),
+  block_physi(nullptr),
+  layer(lyr)
+{}
+
+//_______________________________________________________________
+bool PHG4SquareTubeDetector::IsInBlock(G4VPhysicalVolume * volume) const
+{
+  if (volume == block_physi)
+  {
+    return true;
+  }
+  return false;
+}
+
+namespace {
+	bool PlaceHollowdBox(
+			G4LogicalVolume *mother,
+			G4UserLimits *g4userlimits,
+			bool overlapcheck,
+			const std::string& name,
+			G4Material *mat_inner,
+			double x_inner,
+			double y_inner,
+			double z_inner,
+			G4ThreeVector d_inner,
+			G4Material *mat_outer,
+			double x_outer,
+			double y_outer,
+			double z_outer,
+			G4ThreeVector d_outer
+			)
+	{
+		G4VSolid *all_solid = new G4Box((name+"_all").c_str(),
+				x_outer/2,
+				y_outer/2,
+				z_outer/2);
+
+		G4VSolid *inner_solid = new G4Box((name+"_inner").c_str(),
+				x_inner/2,
+				y_inner/2,
+				z_inner/2+0.1*cm);
+
+		G4VSolid * outer_solid = new G4SubtractionSolid((name+"_outer").c_str(),
+				all_solid,
+				inner_solid,
+				0,
+				d_inner - d_outer
+				);
+
+		G4VisAttributes *vis_outer = new G4VisAttributes();
+		PHG4Utils::SetColour(vis_outer, mat_outer->GetName());
+		vis_outer->SetVisibility(true);
+		vis_outer->SetForceSolid(true);
+
+		G4LogicalVolume *outer_logic = new G4LogicalVolume(outer_solid,
+				mat_outer,
+				(name+"_outer").c_str(),
+				nullptr, nullptr, g4userlimits);
+
+		outer_logic->SetVisAttributes(vis_outer);
+
+		new G4PVPlacement(
+				0,
+				d_outer,
+				outer_logic,
+				(name+"_outer").c_str(),
+				mother, 0, false, overlapcheck);
+
+		return true;
+	}
+}
+//_______________________________________________________________
+void PHG4SquareTubeDetector::Construct( G4LogicalVolume* logicWorld )
+{
+	double inch = 2.54*cm;
+
+	double place_x = params->get_double_param("place_x")*cm;
+	double place_y = params->get_double_param("place_y")*cm;
+	double place_z = params->get_double_param("place_z")*cm;
+
+	double inner_place_x = params->get_double_param("inner_place_x")*cm;
+	double inner_place_y = params->get_double_param("inner_place_y")*cm;
+
+	double size_x = params->get_double_param("size_x")*cm;
+	double size_y = params->get_double_param("size_y")*cm;
+	double size_z = params->get_double_param("size_z")*cm;
+	double inner_size_x = params->get_double_param("inner_size_x")*cm;
+	double inner_size_y = params->get_double_param("inner_size_y")*cm;
+
+  double steplimits = params->get_double_param("steplimits") * cm;
+
+  G4UserLimits *g4userlimits = nullptr;
+  if (isfinite(steplimits))
+  {
+    g4userlimits = new G4UserLimits(steplimits);
+  }
+
+  PlaceHollowdBox(
+  		logicWorld,
+			g4userlimits,
+			overlapcheck,
+			GetName(),
+
+			0,
+			inner_size_x, inner_size_y, size_z,
+			G4ThreeVector(place_x+inner_place_x,place_y+inner_place_y,0.),
+
+			G4Material::GetMaterial(params->get_string_param("material")),
+			size_x, size_y, size_z,
+			G4ThreeVector(place_x, place_y, place_z)
+			);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
