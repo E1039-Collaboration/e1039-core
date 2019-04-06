0758d6da715dc2e13fb6a9023cc6ca3ed2072162
Kenichi Nakano <knakano@nucl.phys.titech.ac.jp>
2019-04-06 00:40:31 -0500

    Removed a part of "setup-e1039-core.sh" since the part is taken care of by the upstream, "this-share.sh".

--- setup-e1039-core.sh
+++ setup-e1039-core.sh
@@ -4,11 +4,10 @@ if [ $HOSTNAME = 'seaquestdaq01.fnal.gov' ] ; then
     export OFFLINE_MAIN=$DIR_E1039_SHARE/inst
     export MY_INSTALL=~/tmp/e1039-core-dev-root6
 
-    export            PATH=$MY_INSTALL/bin:$OFFLINE_MAIN/bin:$PATH
-    export           CPATH=$MY_INSTALL/include:$OFFLINE_MAIN/include:$CPATH
-    export    LIBRARY_PATH=$MY_INSTALL/lib:$OFFLINE_MAIN/lib:$LIBRARY_PATH
-    export LD_LIBRARY_PATH=$MY_INSTALL/lib:$OFFLINE_MAIN/lib:$LD_LIBRARY_PATH
-    
+    export            PATH=$MY_INSTALL/bin:$PATH
+    export           CPATH=$MY_INSTALL/include:$CPATH
+    export    LIBRARY_PATH=$MY_INSTALL/lib:$LIBRARY_PATH
+    export LD_LIBRARY_PATH=$MY_INSTALL/lib:$LD_LIBRARY_PATH
 else
     echo "Using the non-host-specific setting.  This might not work on your environment."
     export  CC=gcc-4.9
