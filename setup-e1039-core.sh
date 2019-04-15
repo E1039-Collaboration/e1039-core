42167eb809a7a69fb91e9fe7399d0d319df9e87c
Kenichi Nakano <knakano@nucl.phys.titech.ac.jp>
2019-04-14 19:46:14 -0500

    I have modified a few subdirectories.  But this version doesn't work despite my expectation.

--- setup-e1039-core.sh
+++ setup-e1039-core.sh
@@ -1,8 +1,9 @@
+DIR_E1039_CORE=$(dirname $(readlink -f $BASH_SOURCE))
 
 if [ $HOSTNAME = 'seaquestdaq01.fnal.gov' ] ; then
     source /opt/e1039-share/this-share.sh
     export OFFLINE_MAIN=$DIR_E1039_SHARE/inst
-    export MY_INSTALL=~/tmp/e1039-core
+    export MY_INSTALL=$(dirname $DIR_E1039_CORE)/e1039-core-build/inst # ~/tmp/e1039-core
 
     export            PATH=$MY_INSTALL/bin:$PATH
     export           CPATH=$MY_INSTALL/include:$CPATH
@@ -18,3 +19,21 @@ else
     export MY_INSTALL=$OFFLINE_MAIN
     export LD_LIBRARY_PATH=$OFFLINE_MAIN/lib:$LD_LIBRARY_PATH
 fi
+
+function cmake-e1039-core {
+    echo "DIR_E1039_CORE = $DIR_E1039_CORE"
+    echo "MY_INSTALL     = $MY_INSTALL"
+
+    cmake -DCMAKE_INSTALL_PREFIX=$MY_INSTALL $DIR_E1039_CORE
+
+    #mkdir -p $build/$package
+    #cd $build/$package
+    #echo cmake -DCMAKE_INSTALL_PREFIX=$install $src/$package
+    #cmake -DCMAKE_INSTALL_PREFIX=$install $src/$package
+    #make -j4 install
+    #ret=$?
+    #if [ $ret -ne 0 ] ; then
+    #	echo "Abort since ret = $ret."
+    #	exit
+    #fi
+}
