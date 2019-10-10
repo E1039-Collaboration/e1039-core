633cd41ee36062825ae4303e89d6f982c49527fa
abinashpun <apun@spinquestgpvm01.fnal.gov>
2019-10-10 15:57:00 -0500

    primary version for review

--- generators/E906LegacyGen/E906VertexGen.h
+++ generators/E906LegacyGen/E906VertexGen.h
@@ -4,10 +4,12 @@
 #include <vector>
 #include <TString.h>
 #include <TGeoManager.h>
+#include <TGeoMedium.h>
 #include <TGeoNode.h>
 #include <TF2.h>
 #include <TVector3.h>
 #include <TH1F.h>
+
 class PHCompositeNode;
 class BeamlineObject;
 
@@ -26,10 +28,12 @@ public:
 
     //Tree traversal
     void traverse(TGeoNode* node, double&xvertex,double&yvertex,double&zvertex);
-    //void traverse(TGeoNode* node, int level);
+
 
     //get the vertex generated
-    TVector3 generateVertex();
+    // TVector3 generateVertex();
+    double generateVertex();
+
     //use the beam profile to generate
     void generateVtxPerp(double& x, double& y);
 
@@ -44,7 +48,7 @@ private:
     unsigned int nPieces;
     double probSum;
     double accumulatedProbs[100]; //for now set to no more than 100 objects
-
+ 
       //vector of the interactable stuff
     std::vector<BeamlineObject> interactables;
 
@@ -57,6 +61,9 @@ private:
 
     //flag to test if the generator has been initialized
     bool inited;
+
+    // flag to use beamProfile function
+    bool beam_profile;
 
   
 };
