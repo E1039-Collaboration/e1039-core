2ec7ed94a434d74f2a34e5d696531d028b0037c9
abinashpun <apun@spinquestgpvm01.fnal.gov>
2019-10-10 15:56:21 -0500

    primary version for review

--- generators/E906LegacyGen/E906LegacyGen.C
+++ generators/E906LegacyGen/E906LegacyGen.C
@@ -1,6 +1,4 @@
 #include "E906LegacyGen.h"
-
-
 #include "E906VertexGen.h"
 #include "BeamlineObject.h"
 #include <TGeoMaterial.h>
@@ -21,10 +19,9 @@ E906LegacyGen::E906LegacyGen(const std::string& name)
   : SubsysReco("E906LegacyGen")
 {
     _vertexGen = new E906VertexGen();
-    //_beamlineobj = new BeamlineObject();
     _out_name = name;
     const TGeoMaterial* pMaterial;
-    //const TGeoMixture* pMaterial;
+
     ResetVars();
     return;
 }
@@ -39,14 +36,12 @@ int E906LegacyGen::Init(PHCompositeNode* topNode)
 {
  
   file = new TFile(_out_name.c_str(), "RECREATE");
-  //histo = new TH1F();
   InitTree(); 
   return Fun4AllReturnCodes::EVENT_OK;
 }
 
 int E906LegacyGen::InitRun(PHCompositeNode* topNode)
 {
-  //PHTFileServer::get().open(_out_name.c_str(), "RECREATE");
   return Fun4AllReturnCodes::EVENT_OK;
 }
 
@@ -59,14 +54,13 @@ int E906LegacyGen::process_event(PHCompositeNode* topNode)
     y_vtx=0.;
     z_vtx=0.;
     _vertexGen->traverse(geoManager->GetTopNode(),x_vtx,y_vtx, z_vtx);
-    // _vertexGen->traverse(geoManager->GetTopNode(), 0);
+    
     truth_vtxx = x_vtx;
     truth_vtxy = y_vtx;
     truth_vtxz = z_vtx;
-    //std::cout<<"from process_event, z_vtx: "<<z_vtx<<std::endl;
-    //histo->Fill(z_vtx);
+
     truth_tree->Fill();
-    // delete _vertexGen;
+ 
     return Fun4AllReturnCodes::EVENT_OK;
 }
 
@@ -79,16 +73,13 @@ int E906LegacyGen::End(PHCompositeNode* topNode)
 {
   file->Write();
   file->Close();
-  //truth_tree->Write();
-  //PHTFileServer::get().cd(_out_name.c_str());
   return Fun4AllReturnCodes::EVENT_OK;
 }
 void E906LegacyGen::ResetVars()
 {
-  // // truth_vtxx= 0.0;
-  // // truth_vtxy=0.0;
+  truth_vtxx= 0.0;
+  truth_vtxy=0.0;
   truth_vtxz=0.0;
-  // histo=0;
 }
 
 void E906LegacyGen::InitTree()
@@ -99,3 +90,4 @@ void E906LegacyGen::InitTree()
    truth_tree->Branch("truth_vtxz", &truth_vtxz, "truth_vtxz/F");
   
 }
+
