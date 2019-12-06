ae46123dd1e4f23471e51dc688a0b50b1c144712
abinashpun <ap564413@ohio.edu>
2019-12-06 10:09:53 -0600

    Delete E1039PhysicsGenLinkDef.h

--- generators/E906LegacyGen/E1039PhysicsGenLinkDef.h
+++
@@ -1,9 +0,0 @@
-#ifdef __CINT__
-
-#pragma link C++ class E1039PhysicsGen-!;
-#pragma link C++ class E906VertexGen-!;
-#pragma link C++ class BeamlineObject-!;
-//#pragma link C++ class MCDimuon.h+;
-//#pragma link C++ class SQDimuonTruthInfoContainer+;
-
-#endif
