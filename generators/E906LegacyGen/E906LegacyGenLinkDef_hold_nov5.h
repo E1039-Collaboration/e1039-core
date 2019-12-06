867e997fddc971a77b654ccbfe4c87964b54cea1
abinashpun <ap564413@ohio.edu>
2019-12-06 10:10:24 -0600

    Delete E906LegacyGenLinkDef_hold_nov5.h

--- generators/E906LegacyGen/E906LegacyGenLinkDef_hold_nov5.h
+++
@@ -1,8 +0,0 @@
-#ifdef __CINT__
-
-#pragma link C++ class E906LegacyGen-!;
-#pragma link C++ class E906VertexGen-!;
-#pragma link C++ class E1039PhysicsGen-!;
-#pragma link C++ class BeamlineObject-!;
-
-#endif
