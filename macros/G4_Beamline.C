f7d3463ab9d00a26b72e5779c233a15bd37bff19
Kun Liu <liuk.pku@gmail.com>
2020-05-22 18:46:43 -0500

    re-organized the spectrometer construction
    1. Created a G4_beamline.C to contain all beamline-related stuff, so far we only have a collimator, in the future we should have beampipe, beam windows and beamline instruments included;
    2. Removed collimator from G4_target to G4_beamline;
    3. Removed shielding from G4_target to G4_insensitivevolumes
    4. Added a Fun4DumpGeom.C to dump the geometry to a ROOT file and (optionally) display it in opengl

---
+++ macros/G4_Beamline.C
@@ -0,0 +1,25 @@
+#pragma once
+#if ROOT_VERSION_CODE >= ROOT_VERSION(6,00,0)
+#include <g4detectors/PHG4CollimatorSubsystem.h>
+class SubsysReco;
+R__LOAD_LIBRARY(libg4detectors)
+#endif
+
+void SetupBeamline(
+  PHG4Reco* g4Reco,
+  const bool toggle_collimator = true,
+  const double collimator_pos_z = -300,
+  const int register_hits = 1) 
+{
+  if(toggle_collimator) {
+    PHG4CollimatorSubsystem* collimator = new PHG4CollimatorSubsystem("Collimator",0);
+    collimator->SuperDetector("Collimator");
+    collimator->set_double_param("place_z", collimator_pos_z);//-302.36 cm
+    collimator->set_double_param("size_z", 121.92);
+    collimator->SetActive(register_hits);
+    g4Reco->registerSubsystem(collimator);
+  }
+ 
+  return;
+}
+
--- macros/G4_InsensitiveVolumes.C
+++ macros/G4_InsensitiveVolumes.C
@@ -1,21 +1,73 @@
 #pragma once
 #if ROOT_VERSION_CODE >= ROOT_VERSION(6,00,0)
 #include <g4detectors/PHG4BlockSubsystem.h>
+#include <g4detectors/PHG4SquareTubeSubsystem.h>
 #include <g4detectors/SQG4DipoleMagnetSubsystem.h>
 class SubsysReco;
 R__LOAD_LIBRARY(libg4detectors)
 #endif
 
+/*
+This macro implements the all the volumes that are not sensitive - i.e. do not need to generate G4Hits
+The insensitive volumes implemented here are:
+    1. concrete shielding in front of FMag
+    2. FMag/beam dump
+    3. KMag
+    4. Muon absorber between station-3 and -4
+*/
+
 void SetupInsensitiveVolumes(
   PHG4Reco* g4Reco,
-  const int toggle_fmag = 1,
-  const int toggle_kmag = 1,
-  const int toggle_absorber = 1,
-  const int enable_fmag_filter = 0,   /*should set this to 1 after being extensively tested*/
+  const bool toggle_shielding = true,
+  const bool toggle_fmag = true,
+  const bool toggle_kmag = true,
+  const bool toggle_absorber = true,
+  const int enable_fmag_filter = 0,      /*should set this to 1 after being extensively tested*/
   const double filter_max_slope = 0.25,
   const double filter_min_energy = 5.)
 {
-  if(toggle_fmag == 1) {
+  if(toggle_shielding) {
+    const double inch = 2.54;
+    PHG4SquareTubeSubsystem* shielding = nullptr;
+
+    shielding = new PHG4SquareTubeSubsystem("Shielding1", 0);
+    shielding->set_string_param("hole_type", "circle");
+    shielding->set_double_param("place_x", 0);
+    shielding->set_double_param("place_y", 0);
+    shielding->set_double_param("place_z", (-18.*inch/2.-(2.15+11.38+36.)*inch)); // I have added all the z length and put the z into the center of mass of the block.
+    shielding->set_double_param("size_x", 250.*inch); //the info is not given?
+    shielding->set_double_param("size_y", 200.*inch); //the info is not given?
+    shielding->set_double_param("size_z", (18.-0.001)*inch);
+    shielding->set_double_param("inner_diameter", 4.*inch);
+    shielding->set_string_param("material", "G4_CONCRETE");
+    g4Reco->registerSubsystem(shielding);
+
+    shielding = new PHG4SquareTubeSubsystem("Shielding2", 0);
+    shielding->set_string_param("hole_type", "circle");
+    shielding->set_double_param("place_x", 0.);
+    shielding->set_double_param("place_y", 0.);
+    shielding->set_double_param("place_z", (-36.*inch/2.-(2.15+11.38)*inch)); // I have added all the z length and put the z into the center of mass of the block.
+    shielding->set_double_param("size_x", 250.*inch); //the info is not given?
+    shielding->set_double_param("size_y", 200.*inch); //the info is not given?
+    shielding->set_double_param("size_z", 36.*inch);
+    shielding->set_double_param("inner_diameter", 6.*inch);
+    shielding->set_string_param("material", "G4_CONCRETE");
+    g4Reco->registerSubsystem(shielding);
+
+    shielding = new PHG4SquareTubeSubsystem("Shielding3", 0);
+    shielding->set_double_param("place_x", 0.);
+    shielding->set_double_param("place_y", 0.);
+    shielding->set_double_param("place_z", -11.38*inch/2.);
+    shielding->set_double_param("size_x", 50.*inch);
+    shielding->set_double_param("size_y", 50.*inch);
+    shielding->set_double_param("size_z", (11.38-0.001)*inch);
+    shielding->set_double_param("inner_size_x", 6.*inch);
+    shielding->set_double_param("inner_size_y", 6.*inch);
+    shielding->set_string_param("material", "G4_CONCRETE");
+    g4Reco->registerSubsystem(shielding);
+  }
+
+  if(toggle_fmag) {
     SQG4DipoleMagnetSubsystem* fmag = new SQG4DipoleMagnetSubsystem("fmag");
     fmag->set_string_param("geomdb", "$E1039_RESOURCE/geometry/magnetic_fields/magnet_geom.db");
     fmag->set_string_param("magname", "fmag");
@@ -31,7 +83,7 @@ void SetupInsensitiveVolumes(
     g4Reco->registerSubsystem(fmag);
   }
 
-  if(toggle_kmag == 1) {
+  if(toggle_kmag) {
     SQG4DipoleMagnetSubsystem* kmag = new SQG4DipoleMagnetSubsystem("fmag");
     kmag->set_string_param("geomdb", "$E1039_RESOURCE/geometry/magnetic_fields/magnet_geom.db");
     kmag->set_string_param("magname", "kmag");
@@ -42,7 +94,7 @@ void SetupInsensitiveVolumes(
     g4Reco->registerSubsystem(kmag);
   }
 
-  if(toggle_absorber == 1) {
+  if(toggle_absorber) {
     PHG4BlockSubsystem* absorber = new PHG4BlockSubsystem("MUID_absorber", 0);
     absorber->SuperDetector("MUID_absorber");
     absorber->set_double_param("size_x", 320.04);
