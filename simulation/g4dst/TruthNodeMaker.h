229785f15f5eba3856f3d449c7aa3b2ef85737c5
Kenichi Nakano <knakano@nucl.phys.titech.ac.jp>
2020-06-08 01:29:15 -0500

    TruthNodeMaker now sets the positions and momenta of tracks at St. 1 and 3.  In order to obtain the at-station info, the track info is now extracted from G4TruthInfo, instead of HepMC::GenParticle.

--- simulation/g4dst/TruthNodeMaker.h
+++ simulation/g4dst/TruthNodeMaker.h
@@ -1,6 +1,8 @@
 #ifndef _TRUTH_NODE_MAKER__H_
 #define _TRUTH_NODE_MAKER__H_
 #include <fun4all/SubsysReco.h>
+class TVector3;
+class TLorentzVector;
 namespace HepMC { 
   class GenParticle; 
 };
@@ -8,15 +10,17 @@ class PHHepMCGenEventMap;
 class PHG4TruthInfoContainer;
 class PHG4HitContainer;
 class SQMCEvent;
+class SQTrack;
 class SQTrackVector;
 class SQDimuonVector;
 
-/// An SubsysReco module to create a set of (compact) nodes for the simulation true info.
+/// An SubsysReco module to create a set of SQ nodes for the simulation true info.
 class TruthNodeMaker: public SubsysReco {
   PHHepMCGenEventMap* genevtmap;
   PHG4TruthInfoContainer* g4true;
   PHG4HitContainer *g4hc_d1x;
-  PHG4HitContainer *g4hc_d3x;
+  PHG4HitContainer *g4hc_d3px;
+  PHG4HitContainer *g4hc_d3mx;
 
   SQMCEvent*      m_evt;
   SQTrackVector*  m_vec_trk;
@@ -33,7 +37,7 @@ class TruthNodeMaker: public SubsysReco {
  private:
   int  GetNodes(PHCompositeNode *topNode);
   int MakeNodes(PHCompositeNode *topNode);
-  //void SetTrackParamUsingHits(const HepMC::GenParticle* par, SQTrack* trk);
+  bool FindHitAtStation(const int trk_id, const PHG4HitContainer* g4hc, TVector3* pos, TLorentzVector* mom);
 };
 
 #endif /* _TRUTH_NODE_MAKER__H_ */
