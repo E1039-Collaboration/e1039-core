7e8822e679e9e1692bee98048b7765e31fba75d1
Kenichi Nakano <knakano@nucl.phys.titech.ac.jp>
2021-01-11 09:55:13 -0600

    I modified "packages/Display/macro/EventDisp4MainDaqDst.C" and related files, because the online event display was not working recently.  It was because the geometry setting (i.e. G4_Target.C etc.) had not been updated.

--- packages/Display/macro/EventDisp4MainDaqDst.C
+++ packages/Display/macro/EventDisp4MainDaqDst.C
@@ -1,6 +1,7 @@
 #if ROOT_VERSION_CODE >= ROOT_VERSION(6,00,0)
-#include "G4_SensitiveDetectors.C"
-#include "G4_Target.C"
+#include <top/G4_Target.C>
+#include <top/G4_InsensitiveVolumes.C>
+#include <top/G4_SensitiveDetectors.C>
 R__LOAD_LIBRARY(libinterface_main)
 R__LOAD_LIBRARY(libfun4all)
 R__LOAD_LIBRARY(libdecoder_maindaq)
@@ -27,9 +28,6 @@ void InitInput()
   Fun4AllServer* se = Fun4AllServer::instance();
   se->Verbosity(0);
 
-  JobOptsSvc *jobopt_svc = JobOptsSvc::instance();
-  jobopt_svc->init("run6_data.opts");
-
   PHG4Reco *g4Reco = new PHG4Reco();
   se->registerSubsystem(g4Reco);
   g4Reco->SetWorldSizeX(1000);
@@ -39,11 +37,8 @@ void InitInput()
   g4Reco->SetWorldMaterial("G4_AIR"); //G4_Galactic, G4_AIR
   g4Reco->SetPhysicsList("FTFP_BERT");
 
-  PHG4E1039InsensSubsystem* insens = new PHG4E1039InsensSubsystem("Insens");
-  g4Reco->registerSubsystem(insens);
-
+  SetupInsensitiveVolumes(g4Reco);
   SetupTarget(g4Reco);
-
   SetupSensitiveDetectors(g4Reco);
 
   se->registerSubsystem(new EvtFilter());
