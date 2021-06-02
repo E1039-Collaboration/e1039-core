1a39faef1371f632489cc39089079edad223fc25
abinashpun <apun@spinquestgpvm01.fnal.gov>
2021-06-02 13:58:32 -0500

    isolating legacy vertex generator

--- generators/E906LegacyGen/SQPrimaryGenLinkDef.h
+++ generators/E906LegacyGen/SQPrimaryGenLinkDef.h
@@ -2,8 +2,5 @@
 #ifdef __CINT__
 
 #pragma link C++ class SQPrimaryParticleGen-!;
-#pragma link C++ class SQExternalGen-!;
-//#pragma link C++ class SQPrimaryVertexGen-!;
-//#pragma link C++ class SQBeamlineObject-!;
 
 #endif
