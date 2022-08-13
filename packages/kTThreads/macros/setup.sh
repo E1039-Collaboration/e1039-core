0edda5dcce003370296f2d0f2f5282f06190f788
Kenichi Nakano <knakano@nucl.phys.titech.ac.jp>
2022-08-13 18:37:55 -0500

    Changed the default setup script.

--- packages/kTThreads/macros/setup.sh
+++ packages/kTThreads/macros/setup.sh
@@ -1,7 +1,7 @@
 export DIR_TOP=$(dirname $(readlink -f $BASH_SOURCE))
 
-#source /data2/e1039/this-e1039.sh
-source /data2/users/kenichi/e1039/online/core-inst/this-e1039.sh
+source /data2/e1039/this-e1039.sh
+#source /data2/users/kenichi/e1039/online/core-inst/this-e1039.sh
 
 export DIR_ONLINE=$E1039_ROOT/online
 export STATUS_DB=$DIR_ONLINE/data_status.db
