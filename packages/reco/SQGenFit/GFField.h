c4d030938678ce0330be1414ebc6b1be20c1e101
Kun Liu <liuk.pku@gmail.com>
2024-09-23 13:45:53 -0600

    Added an interface in SQGenFit::GFField to allow for bend-plane adjustment (specifically for FMag)

--- packages/reco/SQGenFit/GFField.h
+++ packages/reco/SQGenFit/GFField.h
@@ -17,12 +17,14 @@ public:
   TVector3 get(const TVector3& pos) const;
   void get(const double& posX, const double& posY, const double& posZ, double& Bx, double& By, double& Bz) const;
 
-  void setScale(double scale) { _scale = scale; }
+  void setScale(double scale   = 1.) { _scale = scale; }
+  void setOffset(double offset = 0.) { _offset = offset; }
   void disable() { _disable = true; }
 
 private:
   const PHField* _field;
   double _scale;
+  double _offset;
   bool   _disable;
 
 };
