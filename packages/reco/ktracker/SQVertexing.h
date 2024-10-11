bd792e3c7d08b2d83efd46e655fe535fbb703a23
Kun Liu <liuk.pku@gmail.com>
2024-10-11 07:18:05 -0600

    Added a switch in SQVertexing to enable refitting every single muon and update target chi2/pos/mom information

--- packages/reco/ktracker/SQVertexing.h
+++ packages/reco/ktracker/SQVertexing.h
@@ -27,6 +27,8 @@ public:
   int End(PHCompositeNode* topNode);
 
   void set_legacy_rec_container(const bool enable = true)  { legacyContainer = enable; }
+  void set_single_retracking(const bool enable = true)     { enableSingleRetracking = true; }
+
   void set_geom_file_name(const std::string& geomFileName) { geom_file_name = geomFileName; }
 
 private:
@@ -37,11 +39,13 @@ private:
 
   double swimTrackToVertex(SQGenFit::GFTrack& track, double z, TVector3* pos = nullptr, TVector3* mom = nullptr);
   double refitTrkToVtx(SQGenFit::GFTrack& track, double z, TVector3* pos = nullptr, TVector3* mom = nullptr);
+  double refitTrkToVtx(SRecTrack*         track, double z, TVector3* pos = nullptr, TVector3* mom = nullptr);
   double findDimuonZVertex(SRecDimuon& dimuon, SQGenFit::GFTrack& track1, SQGenFit::GFTrack& track2);
   double calcZsclp(double p);
   bool   processOneDimuon(SRecTrack* track1, SRecTrack* track2, SRecDimuon& dimuon);
 
   bool legacyContainer;
+  bool enableSingleRetracking;
 
   int charge1, charge2;
 
